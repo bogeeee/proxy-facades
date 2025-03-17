c77050ed90d6c5ac904f5ea35f860db151e50b11
Boris Gingold <bogeee@bogitech.de>
2025-03-17 17:59:31 +0100

    Add Index files / properly export symbols to esm+cjs

---
+++ index.ts
@@ -0,0 +1,7 @@
+export {RecordedRead, RecordedReadOnProxiedObject, ClassTrackingConfiguration, dualUseTracker_callOrigMethodOnTarget, IteratorReturningProxiedValue, GetIteratorValueProxiedFn, DualUseTracker, ForWatchedProxyHandler} from "./common"
+export {ProxyFacade, FacadeProxyHandler, invalidateObject, getGlobalOrig} from "./proxyFacade";
+export {deleteProperty, objectHasChangeTrackerInstalled, installChangeTracker} from "./origChangeTracking"
+export {RecordedArrayValuesRead} from "./class-trackers/Array"
+export {RecordedMap_has, RecordedMap_get, RecordedMapEntriesRead, RecordedMapKeysRead, RecordedMapValuesRead} from "./class-trackers/Map"
+export {RecordedSet_has, RecordedSetValuesRead} from "./class-trackers/Set"
+export {RecordedValueRead, RecordedUnspecificRead, RecordedPropertyRead,RecordedOwnKeysRead, WatchedProxyFacade,  WatchedProxyHandler} from "./watchedProxyFacade";
--- index_esm.mjs
+++ index_esm.mjs
@@ -1,6 +1,44 @@
 // A wrapper file for ESM to avoid the 'dual package hazard'. See https://nodejs.org/api/packages.html#approach-1-use-an-es-module-wrapper
 
+import cjsCommon from "./common.js"
+export const RecordedRead = cjsCommon.RecordedRead;
+export const RecordedReadOnProxiedObject = cjsCommon.RecordedReadOnProxiedObject;
+export const ClassTrackingConfiguration = cjsCommon.ClassTrackingConfiguration;
+export const dualUseTracker_callOrigMethodOnTarget = cjsCommon.dualUseTracker_callOrigMethodOnTarget;
+export const IteratorReturningProxiedValue = cjsCommon.IteratorReturningProxiedValue;
+export const GetIteratorValueProxiedFn = cjsCommon.GetIteratorValueProxiedFn;
+export const DualUseTracker = cjsCommon.DualUseTracker;
+export const ForWatchedProxyHandler = cjsCommon.ForWatchedProxyHandler
 
+import cjsProxyFacade from "./proxyFacade"
+export const ProxyFacade = cjsProxyFacade.ProxyFacade;
+export const FacadeProxyHandler = cjsProxyFacade.FacadeProxyHandler;
+export const invalidateObject = cjsProxyFacade.invalidateObject;
+export const getGlobalOrig = cjsProxyFacade.getGlobalOrig;
 
-import cjsIndex from "./index.js"
-export const todo = cjsIndex.todo
+import cjsOrigChangeTracking from "./origChangeTracking"
+export const deleteProperty = cjsOrigChangeTracking.deleteProperty;
+export const objectHasChangeTrackerInstalled = cjsOrigChangeTracking.objectHasChangeTrackerInstalled;
+export const installChangeTracker = cjsOrigChangeTracking.installChangeTracker;
+
+import cjsClassTrackers_Array from "./class-trackers/Array";
+export const RecordedArrayValuesRead = cjsClassTrackers_Array.RecordedArrayValuesRead
+
+import cjsClassTrackers_Map from "./class-trackers/Map";
+export const RecordedMap_has = cjsClassTrackers_Map.RecordedMap_has;
+export const RecordedMap_get = cjsClassTrackers_Map.RecordedMap_get;
+export const RecordedMapEntriesRead = cjsClassTrackers_Map.RecordedMapEntriesRead;
+export const RecordedMapKeysRead = cjsClassTrackers_Map.RecordedMapKeysRead;
+export const RecordedMapValuesRead = cjsClassTrackers_Map.RecordedMapValuesRead;
+
+import cjsClassTrackers_Set from "./class-trackers/Set";
+export const RecordedSet_has = cjsClassTrackers_Set.RecordedSet_has;
+export const RecordedSetValuesRead = cjsClassTrackers_Set.RecordedSetValuesRead;
+
+import cjsWatchedProxyFacade from "./watchedProxyFacade"
+export const RecordedValueRead = cjsWatchedProxyFacade.RecordedValueRead;
+export const RecordedUnspecificRead = cjsWatchedProxyFacade.RecordedUnspecificRead;
+export const RecordedPropertyRead = cjsWatchedProxyFacade.RecordedPropertyRead;
+export const RecordedOwnKeysRead = cjsWatchedProxyFacade.RecordedOwnKeysRead;
+export const WatchedProxyFacade = cjsWatchedProxyFacade.WatchedProxyFacade;
+export const WatchedProxyHandler = cjsWatchedProxyFacade.WatchedProxyHandler;
