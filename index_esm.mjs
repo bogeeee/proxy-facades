bd7de3902d283bb285db576b9561e1cdf1802497
Boris Gingold <bogeee@bogitech.de>
2025-03-20 23:31:55 +0100

    Bugfix: index_esm.mjs

--- index_esm.mjs
+++ index_esm.mjs
@@ -10,32 +10,32 @@ export const GetIteratorValueProxiedFn = cjsCommon.GetIteratorValueProxiedFn;
 export const DualUseTracker = cjsCommon.DualUseTracker;
 export const ForWatchedProxyHandler = cjsCommon.ForWatchedProxyHandler
 
-import cjsProxyFacade from "./proxyFacade"
+import cjsProxyFacade from "./proxyFacade.js"
 export const ProxyFacade = cjsProxyFacade.ProxyFacade;
 export const FacadeProxyHandler = cjsProxyFacade.FacadeProxyHandler;
 export const invalidateObject = cjsProxyFacade.invalidateObject;
 export const getGlobalOrig = cjsProxyFacade.getGlobalOrig;
 
-import cjsOrigChangeTracking from "./origChangeTracking"
+import cjsOrigChangeTracking from "./origChangeTracking.js"
 export const deleteProperty = cjsOrigChangeTracking.deleteProperty;
 export const changeTrackedOrigObjects = cjsOrigChangeTracking.changeTrackedOrigObjects;
 export const installChangeTracker = cjsOrigChangeTracking.installChangeTracker;
 
-import cjsClassTrackers_Array from "./class-trackers/Array";
+import cjsClassTrackers_Array from "./class-trackers/Array.js";
 export const RecordedArrayValuesRead = cjsClassTrackers_Array.RecordedArrayValuesRead
 
-import cjsClassTrackers_Map from "./class-trackers/Map";
+import cjsClassTrackers_Map from "./class-trackers/Map.js";
 export const RecordedMap_has = cjsClassTrackers_Map.RecordedMap_has;
 export const RecordedMap_get = cjsClassTrackers_Map.RecordedMap_get;
 export const RecordedMapEntriesRead = cjsClassTrackers_Map.RecordedMapEntriesRead;
 export const RecordedMapKeysRead = cjsClassTrackers_Map.RecordedMapKeysRead;
 export const RecordedMapValuesRead = cjsClassTrackers_Map.RecordedMapValuesRead;
 
-import cjsClassTrackers_Set from "./class-trackers/Set";
+import cjsClassTrackers_Set from "./class-trackers/Set.js";
 export const RecordedSet_has = cjsClassTrackers_Set.RecordedSet_has;
 export const RecordedSetValuesRead = cjsClassTrackers_Set.RecordedSetValuesRead;
 
-import cjsWatchedProxyFacade from "./watchedProxyFacade"
+import cjsWatchedProxyFacade from "./watchedProxyFacade.js"
 export const RecordedValueRead = cjsWatchedProxyFacade.RecordedValueRead;
 export const RecordedUnspecificRead = cjsWatchedProxyFacade.RecordedUnspecificRead;
 export const RecordedPropertyRead = cjsWatchedProxyFacade.RecordedPropertyRead;
